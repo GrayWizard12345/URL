a1a076bb5f63fced9b10d2a245849ba9972dadf8
GrayWizard12345 <m.abduganiev@student.inha.uz>
2018-06-27 10:49:51 +0500

    Initial commit

---
+++ src/Starter.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Executable;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Starter {
+
+    public static void main(String[] args) {
+
+              ReverseServlet reverseServlet = new ReverseServlet();
+//            URL url = new URL("http://robokidz.uz/");
+//            URLConnection urlConnection = url.openConnection();
+//
+//            urlConnection.setDoOutput(true);
+//
+//            OutputStreamWriter out =
+//                    new OutputStreamWriter(
+//                            urlConnection.getOutputStream());
+//            out.write("Muslim");
+//            out.close();
+
+        }
+    }
+
